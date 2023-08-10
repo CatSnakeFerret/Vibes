0774fc82645e448ba7dbd57b418fab8f835e68dc
DonoCodePeriod <135299863+DonoCodePeriod@users.noreply.github.com>
2023-08-10 11:29:26 -0400

    rate unreload removed

--- client/components/Rate.jsx
+++ client/components/Rate.jsx
@@ -49,7 +49,7 @@ const Rate = (props) => {
               onClick={() => {
                 // props.setRating(givenRating)
                 setRatingAndState(givenRating);
-                location.reload();
+                //location.reload();
                 // console.log(rating);
               }}
             />
